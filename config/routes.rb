ed6a269b099f39302bb02765fd4fb4453954e8d0
abdul-wahab-qbatch <abdul.wahab@qbatch.com>
2022-08-15 18:09:21 +0500

    Add necessary admin controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :sellers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
