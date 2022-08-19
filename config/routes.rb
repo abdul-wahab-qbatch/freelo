113d86d7421e5589a9401b1382861dcf74904f80
abdul-wahab-qbatch <abdul.wahab@qbatch.com>
2022-08-19 18:26:13 +0500

    Final commit.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  devise_for :admins, controllers: {
+  devise_for :admins, path: '/admin', controllers: {
     sessions: 'admin/sessions'
   }
   devise_for :sellers, controllers: {
