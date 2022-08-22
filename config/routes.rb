50421ecee6a85c2e1190b7e082934a637b0fa1ee
abdul-wahab-qbatch <abdul.wahab@qbatch.com>
2022-08-22 10:47:52 +0500

    Add necessary seller and buyer model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
   devise_for :admins, path: '/admin', controllers: {
     sessions: 'admin/sessions'
   }
-  # devise_for :sellers, controllers: {
-  #   sessions: 'sellers/sessions',
-  #   registrations: 'sellers/registrations'
-  # }
-  # devise_for :buyers, controllers: {
-  #   sessions: 'buyers/sessions',
-  #   registrations: 'buyers/registrations'
-  # }
+  devise_for :sellers, controllers: {
+    sessions: 'sellers/sessions',
+    registrations: 'sellers/registrations'
+  }
+  devise_for :buyers, controllers: {
+    sessions: 'buyers/sessions',
+    registrations: 'buyers/registrations'
+  }
 end
